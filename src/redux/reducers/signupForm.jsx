46d8b8ef7f989f313d08ffb878bf3a03b3646598
wayaz107 <wayaz107@gmail.com>
2021-08-16 18:34:25 -0500

    create signup form reducer

---
+++ src/redux/reducers/signupForm.jsx
@@ -0,0 +1,10 @@
+
+
+export default function manageSignupForm(state, action) {
+    switch (action.type) {
+        case "UPDATE_SIGNUP_FORM":
+        case "RESET_SIGNUP_FORM":
+        default:
+            return state
+    }
+}
