37c5ccb0db92dd5591030ecb9c6655ef108d9fdb
wayaz107 <wayaz107@gmail.com>
2021-08-16 15:23:27 -0500

    add current user reducer

---
+++ src/redux/reducers/currentUser.jsx
@@ -0,0 +1,20 @@
+const initialState = {
+    user: {},
+    loggedIn: false
+}
+
+export default function manageCurrentUser(state = initialState, action) {
+    switch (action.type) {
+
+        case "SET_CURRENT_USER":
+            return {
+                ...state,
+                user: action.payload,
+                loggedIn: true
+            }
+        case "CLEAR_CURRENT_USER":
+            return null
+        default:
+            return state
+    }
+}
