1618191ddee427145009d69a721ed2190631d768
wayaz107 <wayaz107@gmail.com>
2021-08-16 15:25:46 -0500

    edit login form reducer

--- src/redux/reducers/loginForm.jsx
+++ src/redux/reducers/loginForm.jsx
@@ -1,9 +1,15 @@
+const initialState = {
+    email: "",
+    password: ""
+}
 
-
-export default function manageLoginForm(state, action) {
+export default function manageLoginForm(state = initialState, action) {
     switch (action.type) {
         case "UPDATE_LOGIN_FORM":
+            return action.formData
         case "RESET_LOGIN_FORM":
+            return initialState
         default:
+            return state
     }
 }
