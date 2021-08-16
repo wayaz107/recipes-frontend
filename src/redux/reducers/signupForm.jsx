548141aee9fd5c04ada3fde8f71c07afb0babd93
wayaz107 <wayaz107@gmail.com>
2021-08-16 18:37:54 -0500

    edit signup form reducer

--- src/redux/reducers/signupForm.jsx
+++ src/redux/reducers/signupForm.jsx
@@ -1,9 +1,15 @@
+const initialState = {
+    name: "",
+    email: "",
+    password: ""
+}
 
-
-export default function manageSignupForm(state, action) {
+export default function manageSignupForm(state = initialState, action) {
     switch (action.type) {
         case "UPDATE_SIGNUP_FORM":
+            return action.formData
         case "RESET_SIGNUP_FORM":
+            return initialState
         default:
             return state
     }
