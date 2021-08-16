40b39f945e3df9fed83f83346d3e06ff964c4ca6
wayaz107 <wayaz107@gmail.com>
2021-08-16 15:25:08 -0500

    add login form reducer

---
+++ src/redux/reducers/loginForm.jsx
@@ -0,0 +1,9 @@
+
+
+export default function manageLoginForm(state, action) {
+    switch (action.type) {
+        case "UPDATE_LOGIN_FORM":
+        case "RESET_LOGIN_FORM":
+        default:
+    }
+}
