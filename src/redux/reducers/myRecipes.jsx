5a43dad7ee17b6f7a986850035920ca8a16827b1
wayaz107 <wayaz107@gmail.com>
2021-09-14 13:53:58 -0500

    edited recipe reducer

--- src/redux/reducers/myRecipes.jsx
+++ src/redux/reducers/myRecipes.jsx
@@ -6,5 +6,9 @@ const initialState = {
 export default function manageRecipes(state = initialState, action) {
     switch (action.type) {
         case "SET_ALL_RECIPES":
+            return {
+                ...state,
+                allRecipes: action.allRecipes
+            }
     }
 }
